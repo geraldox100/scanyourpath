ee34594fa75f843f106cc5487df947981278d0b1
Geraldo Ferraz <geraldox100@gmail.com>
2015-06-19 22:01:59 -0300

    This resolves #17

--- src/main/java/br/com/geraldoferraz/scanyourpath/Scanner.java
+++ src/main/java/br/com/geraldoferraz/scanyourpath/Scanner.java
@@ -11,6 +11,13 @@ import br.com.geraldoferraz.scanyourpath.searches.loaders.ClassPathLoader;
 /**
  * 
  * @author Geraldo Ferraz
+ * The Scanner class is the starting point for classpath scanning.
+ * Example:
+ * 		Scanner scan = new Scanner();
+ * 		scan.allClasses().anyWhere();
+ * 
+ * For a more refined search try using a search argument and a limiting a path to search: 
+ * 		Set<Class<?>> classes = scan.allClasses(annotedOnClassWith(Entity.class)).exactlyIn("br.com.beans");
  *
  */
 public class Scanner {
@@ -19,16 +26,30 @@ public class Scanner {
 
 	private ClassPathResolver resolver = ClassPathResolver.newInstance(new JavaClassPathResolver());
 
+	/**
+	 * This method tells the scanner that all classes must match the given argument.
+	 * @param argument The argument that all classes must match
+	 * @return A object that allows you to tell the scanner where to search.
+	 */
 	public Location allClasses(Argument argument) {
 		searchType = new SearchByArguments(argument);
 		return new Location(searchType,resolver);
 	}
 
+	/**
+	 * This method tells the scanner to search for all classes
+	 * @return A object that allows you to tell the scanner where to search. 
+	 */
 	public Location allClasses() {
 		searchType = new SearchAll();
 		return new Location(searchType,resolver);
 	}
 
+	/**
+	 * This method tells the scanner the classpath type (Jar, Folder, Full).
+	 * @param classPathSearchType The classpath type to be used.
+	 * @see {@link br.com.geraldoferraz.scanyourpath.searches.loaders.ClassPathLoaderTypes}
+	 */
 	public void limitSearchingPathTo(ClassPathLoader classPathSearchType) {
 		resolver.limitSearchingPathTo(classPathSearchType);
 	}
